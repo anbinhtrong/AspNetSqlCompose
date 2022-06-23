af1cf533acce86a707e17082e53d1289c3de8490
Minh Tam <minhtam.seta@gmail.com>
2022-06-23 21:57:53 +0700

    Initial Project with ASP.NET Core 6 with Bootstrap 5

---
+++ src/ContosoUniversity/ContosoUniversity/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
